92886576b6ae412914462d2bd2174f816c76989d
Ryan M <ryanthemok@gmail.com>
2026-03-22 14:46:58 -0400

    fix redirect error

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://timesup.bike',
+  trailingSlash: 'never',
 
   vite: {
     plugins: [tailwindcss()]
